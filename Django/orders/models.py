eb7b89b9c4e5dc0ccef28e150607360e5affbef2
Matthieu Bleichner <matthieu258@hotmail.com>
2025-02-27 10:53:34 +0100

    create Order model

--- Django/orders/models.py
+++ Django/orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    marketplace = models.fields.CharField(max_length=100)
+    ref_id = models.fields.CharField(max_length=100)
+    date = models.fields.DateField()
+    amount = models.fields.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.fields.CharField(max_length=3)
+
